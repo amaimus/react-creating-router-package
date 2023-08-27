6d8d7041ebb4d4cfb8b362572cf633dc638b4a7f
Alejandro M <alejandromaimus147@gmail.com>
2023-08-27 17:03:49 +0200

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
 
 import { Router } from './components/Router.jsx'
 import { Route } from './components/Route.jsx'
-import NotFoundPage from './pages/NotFound.jsx'
 import { routes } from '../routes'
 
 import HomePage from './pages/Home.jsx'
-import AboutPage from './pages/About.jsx'
+
+const AboutPage = lazy(() => import('./pages/About.jsx'))
+const NotFoundPage = lazy(() => import('./pages/NotFound.jsx'))
 
 export default function App () {
   return (
     <>
       <h1>New react-router</h1>
       <main>
-        <Router routes={routes} defaultComponent={NotFoundPage}>
-          <Route path='/' component={HomePage} />
-          <Route path='/about' component={AboutPage} />
-        </Router>
+        <Suspense fallback={<div>loading...</div>}>
+          <Router routes={routes} defaultComponent={NotFoundPage}>
+            <Route path='/' component={HomePage} />
+            <Route path='/about' component={AboutPage} />
+          </Router>
+        </Suspense>
       </main>
     </>
   )
