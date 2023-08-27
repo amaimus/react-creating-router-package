f40b1186e84f819025b34a5504984f9e0f4a954a
Alejandro M <alejandromaimus147@gmail.com>
2023-08-27 14:04:33 +0200

    handling modified events

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -8,8 +8,14 @@ export function navigate (href) {
 
 export function Link ({ target, to, ...props }) {
   const handleClick = (e) => {
-    e.preventDefault()
-    navigate(to)
+    const isMainEvent = e.button === 0 // primary click
+    const isModifiedEvent = e.metaKey || e.altKey || e.ctrlKey || e.shiftKey
+    const isManageableEvent = target === undefined || target === '_self'
+
+    if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+      e.preventDefault()
+      navigate(to)
+    }
   }
 
   return <a onClick={handleClick} href={to} target={target} {...props} />
