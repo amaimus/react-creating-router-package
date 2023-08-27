0bed553ef62271b0627c367d0e066a394a7b5fcf
Alejandro M <alejandromaimus147@gmail.com>
2023-08-27 17:15:12 +0200

    kind of i18n using routes params

--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
+import AboutPage from './src/pages/About.jsx'
 import SearchPage from './src/pages/Search.jsx'
 
 export const routes = [
+  {
+    path: '/:lang/about',
+    component: AboutPage
+  },
   {
     path: '/search/:query',
     component: SearchPage
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,31 @@
 import { Link } from '../components/Link.jsx'
 
-export default function About () {
+const i18n = {
+  es: {
+    title: 'Sobre mi en:',
+    button: 'Ir a Home',
+    description: 'Esta es la página de Sobre mi'
+  },
+  en: {
+    title: 'About me in:',
+    button: 'Go to Home',
+    description: 'This is the About page'
+  }
+}
+
+const usei18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+export default function About ({ routeParams }) {
+  const i18n = usei18n(routeParams.lang) ?? 'es'
+
   return (
     <>
-      <h1>About</h1>
-      <p>Example About page of react-router</p>
-      <Link to='/'>Go to Home</Link>
+
+      <h1>{i18n.title} {routeParams.lang}</h1>
+      <p>{i18n.description}</p>
+      <Link to='/'>{i18n.button}</Link>
 
     </>
   )
